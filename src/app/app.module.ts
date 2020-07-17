0da1960470c42c9a9d68ec76889f10e4f6700ee7
Akash Jaiswal <akashjaiswal@iiitdmj.ac.in>
2020-07-17 13:13:30 +0530

    Created component header and subcomponents header-navigation & header-content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HeaderNavigationComponent } from './header/header-navigation/header-navigation.component';
+import { HeaderContentComponent } from './header/header-content/header-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeaderNavigationComponent,
+    HeaderContentComponent
   ],
   imports: [
     BrowserModule,
